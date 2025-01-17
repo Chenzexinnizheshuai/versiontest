8e0c6e93de1f300fcbed73f6587051951ceb81a0
czx <chenzexin@tedu.cn>
2025-01-18 03:04:41 +0800

    更改 hash

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const versionPlugin = require('./build/version')
+const cp = require("child_process");
+let commitHash = '0'
+// 执行一个shell命令，使用git rev-parse --short HEAD来获取当前git仓库的commit哈希值
+commitHash = cp.execSync("git rev-parse --short HEAD").toString().trim();
 module.exports = {
   entry: './src/main.js',
   mode: 'development',
@@ -21,7 +25,7 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: `[name]_[contenthash:8].css`
     }),
-    new versionPlugin(+new Date())
+    new versionPlugin(commitHash)
   ],
   output: {
     filename: 'bundle.js',
